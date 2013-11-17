19953fde8991940c083939fcb3c7c8754fa8aecf
shmakovdima <shmakovdima2101@gmail.com>
2013-11-17 17:44:32 +0400

    what???

--- js/auto.js
+++ js/auto.js
@@ -1,1 +1,9 @@
+	jQuery(function()
+    	{
+        	var element = jQuery('#list').jScrollPane({
+        			verticalDragMinHeight: 11,
+        			verticalDragMaxHeight: 11, showArrows: true, hideFocus:true
+					}
+        		);
 
+    	});
