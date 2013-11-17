46519e224d557b3b82caa10e95c9480f46bd990c
Dmitry Shmakov <shmakovdima2101@gmail.com>
2013-11-18 00:24:58 +0400

    еще

--- js/auto.js
+++ js/auto.js
@@ -1,5 +1,20 @@
-jQuery(function()
-    {
-        jQuery('.scroll-pane').jScrollPane();
-    });
+var soglasion = false;
 
+function soglasieclick() {
+	if (soglasion == false){
+		soglasion = true;
+		//alert(soglasion);
+		document.getElementById("soglasietext").innerHTML = '<p  title = "Согласие на обработку персональных данных">Нет, я не согласен(на) на обработку моих данных</p>';
+	}else{
+		soglasion = false;
+		document.getElementById("soglasietext").innerHTML = '<p  title = "Согласие на обработку персональных данных">Да, я согласен(на) на обработку моих данных</p>';
+		//alert(soglasion);
+	}
+}
+
+function submitonclick() {
+	if (soglasion==false) {
+		alert('Вы не подвердили свое согласие на обработку ваших персональных данных');
+		
+	}
+}
